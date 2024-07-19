ccaf428f78339c3b1a9e858f507eefbe1ed33c53
bmV3Qmll <xthanh051423@gmail.com>
2024-07-19 09:05:57 +0700

    Finish Basic Functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/like', to: 'users#like', as: 'like'
 
   resources :posts, except: [:index, :show]
+  post 'fetch', to: 'posts#fetch', as: 'fetch'
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
