3a39d75dc0815af901d9a4da8933f9af47d7006b
bmV3Qmll <xthanh051423@gmail.com>
2024-07-24 10:27:51 +0700

    Fix counters not updated when user is deleted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'fotobook-1.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:         'smtp-relay.brevo.com',
